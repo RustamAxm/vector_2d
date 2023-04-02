ad4a75925188104f3efe1c6f819738d6a6f28ea6
Rustam Akhmadullin <rustamaxm@gmail.com>
2023-04-03 00:54:15 +0300

    vector | ok

--- main.cpp
+++ main.cpp
@@ -13,8 +13,20 @@ int Test2Dvector() {
     Vector2D<int> arr3 = arr2;
     std::cout << arr3.GetRows_size() << " " << arr3.GetCols_size() << std::endl;
 
+    arr3 | arr2;
+    std::cout << arr3.GetRows_size() << " " << arr3.GetCols_size() << std::endl;
+
     std::cout << arr3[5][5] << " " << arr_[5][5] << std::endl;
+    arr3[5][5] = 5;
+    arr_[5][5] = 7;
+    std::cout << arr3[5][5] << " " << arr_[5][5] << std::endl;
+
     std::cout << arr3.GetColumn(7).size() << " " << arr3.GetRow(3).size() << std::endl;
+
+    Array2D_umap<int> arru_(3, 3, 1);
+    Array2D_umap<int> arru_2(arru_);
+    Array2D_umap<int> arru_3 = arru_2;
+
     return 0;
 }
 
--- vector_2d.h
+++ vector_2d.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <utility>
+#include <unordered_map>
 
 
 template <typename Type>
@@ -23,13 +24,8 @@ public:
     }
 
     Vector2D(Vector2D<Type> & other) {
-        std::vector<std::vector<Type>> tmp;
-        for (size_t i = 0; i < other.cols_; ++i) {
-            tmp.push_back(other.GetColumn(i));
-        }
-        std::swap(vector2d_, tmp);
-        std::swap(rows_, other.rows_);
-        std::swap(cols_, other.cols_);
+        auto tmp = copy(other);
+        swap(tmp);
     }
 
     Vector2D(Vector2D<Type> && other) {
@@ -40,16 +36,21 @@ public:
 
     class Proxy {
     public:
-        Proxy(std::vector<Type> vec) : vec_(vec) {}
-        Type operator[](size_t index) {
+        Proxy(std::vector<Type> vec, size_t index) : vec_(vec), index_(index) {}
+
+        Type& operator[](size_t index) {
             return vec_[index];
+        }
+        Type& operator=(const Type & other) {
+            return vec_[index_] = other;
         }
     private:
         std::vector<Type> vec_;
+        size_t index_;
     };
 
     Proxy operator[](size_t index) {
-        return Proxy(vector2d_[index]);
+        return Proxy(vector2d_[index], index);
     }
 
     Vector2D& operator=(const Vector2D& rhs) {
@@ -61,9 +62,17 @@ public:
     }
 
     Vector2D& operator=(Vector2D && rhs) {
-        std::swap(vector2d_, rhs.vector2d_);
-        std::swap(cols_, rhs.cols_);
-        std::swap(rows_, rhs.rows_);
+        swap(rhs);
+        return *this;
+    }
+
+    Vector2D & operator|(const Vector2D & rhs) {
+        for (size_t i = 0; i < cols_ || i < rhs.cols_; ++i) {
+            vector2d_[i].insert(vector2d_[i].end(),
+                             std::make_move_iterator(rhs.vector2d_[i].begin()),
+                             std::make_move_iterator(rhs.vector2d_[i].end()));
+        }
+        cols_ += rhs.cols_;
         return *this;
     }
 
@@ -73,7 +82,7 @@ public:
 
     std::vector<Type> GetRow(size_t index) {
         std::vector<Type> to_return;
-        for (size_t j = 0; j < cols_; ++j) {
+        for (size_t j = 0; j < rows_; ++j) {
             to_return.push_back(vector2d_.at(j).at(index));
         }
         return to_return;
@@ -90,10 +99,12 @@ public:
 private:
 
     Vector2D<Type> copy(const Vector2D & other) {
-        std::vector<std::vector<Type>> tmp;
+        Vector2D tmp;
         for (size_t i = 0; i < other.cols_; ++i) {
-            tmp.push_back(other.GetColumn(i));
+            tmp.vector2d_.push_back(other.GetColumn(i));
         }
+        tmp.cols_ = other.cols_;
+        tmp.rows_ = other.rows_;
         return tmp;
     }
 
@@ -106,4 +117,109 @@ private:
     std::vector<std::vector<Type>> vector2d_;
     size_t cols_ = 0;
     size_t rows_ = 0;
+};
+
+struct index {
+public:
+    index(size_t x, size_t y) : x_(x), y_(y) {}
+    size_t x_;
+    size_t y_;
+    bool operator==(const index & rhs) const {
+        return x_ == rhs.x_ && y_ == rhs.y_;
+    }
+};
+
+class Hasher {
+public:
+    size_t operator()(const index ind) const {
+        size_t h1 = std::hash<size_t>()(ind.x_);
+        return std::hash<size_t>()(ind.y_) ^ (h1 << 1);
+    }
+};
+
+template <typename Type>
+class Array2D_umap {
+public:
+    Array2D_umap() = default;
+    Array2D_umap(size_t m, size_t n) : cols_(m), rows_(n) {
+    }
+    Array2D_umap(size_t m, size_t n, const Type value) : cols_(m), rows_(n) {
+
+        for (size_t i = 0; i < cols_; i++) {
+            for (size_t j = 0; j < rows_; ++j) {
+                index index_(i, j);
+                arr_[index_] = value;
+            }
+        }
+    }
+    Array2D_umap(const Array2D_umap & other) {
+        auto tmp = copy(other);
+        swap(tmp);
+    }
+
+    Array2D_umap(Array2D_umap && other) {
+        arr_ = std::move(other.arr_);
+        cols_ = std::move(other.cols_);
+        rows_ = std::move(other.rows_);
+    }
+
+    Array2D_umap & operator=(const Array2D_umap & rhs) {
+        if (this != rhs) {
+            Array2D_umap rhs_copy(rhs);
+            swap(rhs_copy);
+        }
+        return *this;
+    }
+
+    Array2D_umap & operator=(Array2D_umap && rhs) {
+        swap(rhs);
+        return *this;
+    }
+
+    class Proxy {
+    public:
+        Proxy(std::vector<Type> vec, size_t index) : vec_(vec), index_(index) {}
+
+        Type& operator[](size_t index) {
+            return vec_[index];
+        }
+        Type& operator=(Type other) {
+            return vec_[index_] = other;
+        }
+    private:
+        std::vector<Type> vec_;
+        size_t index_;
+    };
+
+    Proxy operator[](size_t index) {
+        return Proxy();
+    }
+
+    ~Array2D_umap() = default;
+
+private:
+
+    std::unordered_map<index, Type, Hasher> arr_;
+    size_t rows_;
+    size_t cols_;
+
+
+    Array2D_umap copy(const Array2D_umap & other) {
+        Array2D_umap<Type> tmp;
+        for (size_t i = 0; i < other.cols_; i++) {
+            for (size_t j = 0; j < other.rows_; ++j) {
+                index index_(i, j);
+                tmp.arr_[index_] = other.arr_.at(index_);
+            }
+        }
+        tmp.rows_ = other.rows_;
+        tmp.cols_ = other.cols_;
+        return tmp;
+    }
+
+    void swap(Array2D_umap & other) {
+        std::swap(arr_, other.arr_);
+        std::swap(rows_, other.rows_);
+        std::swap(cols_, other.cols_);
+    }
 };
